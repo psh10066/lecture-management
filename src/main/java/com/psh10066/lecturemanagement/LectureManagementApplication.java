cba780a5d53a8163210007e1a23aa3bd0f17177d
이호준 <psh10066@gmail.com>
2024-04-12 14:23:59 +0900

    프로젝트 생성

---
+++ src/main/java/com/psh10066/lecturemanagement/LectureManagementApplication.java
@@ -0,0 +1,13 @@
+package com.psh10066.lecturemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectureManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LectureManagementApplication.class, args);
+    }
+
+}
