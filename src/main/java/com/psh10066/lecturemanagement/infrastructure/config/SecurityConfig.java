6fc7753a82111927af45aaf4f9b512014618e90c
이호준 <psh10066@gmail.com>
2024-04-20 18:59:14 +0900

    로그인 시 rememberMe 적용

--- src/main/java/com/psh10066/lecturemanagement/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/psh10066/lecturemanagement/infrastructure/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.psh10066.lecturemanagement.infrastructure.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,10 +8,17 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final DataSource dataSource;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -23,11 +31,22 @@ public class SecurityConfig {
             .formLogin(login -> login
                 .defaultSuccessUrl("/study")
             )
+            .rememberMe(rememberMe -> rememberMe
+                .tokenRepository(persistentTokenRepository())
+            )
             .authorizeHttpRequests(config -> config
                 .anyRequest().hasRole("USER")
             )
         ;
 
         return http.build();
+    }
+
+    @Bean
+    public PersistentTokenRepository persistentTokenRepository() {
+        JdbcTokenRepositoryImpl jdbcTokenRepository = new JdbcTokenRepositoryImpl();
+        jdbcTokenRepository.setDataSource(dataSource);
+//        jdbcTokenRepository.setCreateTableOnStartup(true); // persistent_logins 테이블 없는 경우 최초 생성 설정
+        return jdbcTokenRepository;
     }
 }
