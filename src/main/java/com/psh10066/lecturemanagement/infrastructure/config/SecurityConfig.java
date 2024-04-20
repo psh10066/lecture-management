d4941c0d13e7fbc906c57d3c59f13fe288844ec8
이호준 <psh10066@gmail.com>
2024-04-21 03:25:02 +0900

    파비콘 적용

--- src/main/java/com/psh10066/lecturemanagement/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/psh10066/lecturemanagement/infrastructure/config/SecurityConfig.java
@@ -1,11 +1,13 @@
 package com.psh10066.lecturemanagement.infrastructure.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -56,5 +58,11 @@ public class SecurityConfig {
         jdbcTokenRepository.setDataSource(dataSource);
 //        jdbcTokenRepository.setCreateTableOnStartup(true); // persistent_logins 테이블 없는 경우 최초 생성 설정
         return jdbcTokenRepository;
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring()
+            .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 }
